7a14b9785e710f995235db0bde9f5cdcdc24540a
Yves Bertot <Yves.Bertot@inria.fr>
2022-02-24 09:27:11 +0100

    a version that uses mathcomp-inspired bigops

--- R_nat_ind.v
+++ R_nat_ind.v
@@ -63,9 +63,7 @@ Lemma Rnat0 : Rnat 0.
 Proof. now exists 0%nat. Qed.
 
 Lemma Rnat_imm (x : positive) : Rnat (IZR (Z.pos x)).
-Proof.
-now exists (Pos.to_nat x); rewrite INR_IPR.
-Qed.
+Proof.  auto.  Qed.
 
 Example Rnat42 : Rnat 42.
 Proof. apply Rnat_imm. Qed.
@@ -78,6 +76,9 @@ Proof.
 intros [x' xnat] [y' ynat]; exists (x' + y')%nat.
 now rewrite xnat, ynat, plus_INR.
 Qed.
+
+#[export]
+Hint Resolve Rnat_add : core.
 
 Lemma Rnat_mul (x y : R) : Rnat x -> Rnat y -> Rnat (x * y).
 Proof.
@@ -132,11 +133,18 @@ assert (tmp2' := tmp2 (S n') nn1'); unfold Rnat_iter; rewrite <- tmp2'.
 easy.
 Qed.
 
+Lemma Rnat_iterS' {A : Type} (n : R) (f : A -> A) (e : A) :
+  Rnat (n - 1) ->
+  Rnat_iter n f e = f (Rnat_iter (n - 1) f e).
+Proof.
+intros nn; replace n with ((n - 1) + 1) at 1 by ring.
+now apply Rnat_iterS.
+Qed.
+
 Lemma Rnat_iter1 {A : Type} (f : A -> A) (e : A) :
   Rnat_iter 1 f e = f e.
 Proof.
-replace 1 with (0 + 1) by ring.
-rewrite Rnat_iterS;[ | exact Rnat0].
+rewrite Rnat_iterS'; rewrite Rminus_eq_0; auto.
 now rewrite Rnat_iter0.
 Qed.
 
@@ -149,81 +157,66 @@ induction nnat using Rnat_ind.
   now rewrite Rplus_0_l, Rnat_iter0.
 replace (k + 1 + m) with (k + m + 1) by ring.
 rewrite !Rnat_iterS; auto.
-  now rewrite IHnnat.
-now apply Rnat_add.
+now rewrite IHnnat.
 Qed.
 
 Definition Rseq_Prop (n m : R) (v : list R) :=
-  forall n' m' : nat, n = INR n' -> m = INR m' ->
-              v = map (fun e => IZR (Z.of_nat e)) (seq n' m').
+  forall m' : nat, m = INR m' ->
+              v = map (fun e => n + IZR (Z.of_nat e)) (seq 0 m').
 
 Definition Rseq (n m : R) : list R :=
   epsilon (inhabits nil) (Rseq_Prop n m).
 
 (* The next two lemmas reproduce the reduction rules of Coq.Lists.List.seq *)
 
-Lemma Rseq0 (n : R) : Rnat n -> Rseq n 0 = nil.
+Lemma Rseq0 (n : R) : Rseq n 0 = nil.
 Proof.
-intros [n' nnat].
 assert (exv : exists v, Rseq_Prop n 0 v).
-  exists (map INR (seq n' 0)); intros n2 z nn2.
+  exists (map (fun e => n + INR e) (seq 0 0)); intros n2.
   change 0 with (INR 0); intros z0; apply INR_eq in z0.
   now rewrite <- z0.
 exact (epsilon_spec (inhabits nil)
-             (Rseq_Prop n 0)
-             exv n' 0%nat nnat refl_equal).
+             (Rseq_Prop n 0) exv 0%nat refl_equal).
 Qed.
 
-Lemma Rseq_S (n m : R) : Rnat n -> Rnat m ->
+Lemma Rseq_S (n m : R) : Rnat m ->
   Rseq n (m + 1) = n :: (Rseq (n + 1) m).
 Proof.
-intros [n' nnat] [m' mnat].
+intros [m' mnat].
 assert (exv1 : exists v, Rseq_Prop (n + 1) m v).
-  exists (map (fun e => IZR (Z.of_nat e)) (seq (S n') m')).
-  intros n'' m''; rewrite nnat, mnat; intros nnat' mnat'.
-  rewrite <- S_INR in nnat'.
-  now apply INR_eq in nnat', mnat'; rewrite <- nnat', mnat'.
+  exists (map (fun e => (n + 1) + IZR (Z.of_nat e)) (seq 0 m')).
+  intros m''; rewrite mnat; intros mnat'.
+  now apply INR_eq in mnat'; rewrite <- mnat'.
 assert (exv2 : exists v, Rseq_Prop n (m + 1) v).
-  exists (map (fun e => IZR (Z.of_nat e)) (seq n' (S m'))).
-  intros n'' m''; rewrite nnat, mnat, <- S_INR; intros nnat' mnat'.
-  now apply INR_eq in nnat', mnat'; rewrite <- nnat', mnat'.
+  exists (map (fun e => n + IZR (Z.of_nat e)) (seq 0 (S m'))).
+  intros m''; rewrite mnat, <- S_INR; intros mnat'.
+  now apply INR_eq in mnat'; rewrite mnat'.
 assert (m1nat : m + 1 = INR (S m')) by now rewrite mnat, <- S_INR.
-assert (n1nat : n + 1 = INR (S n')) by now rewrite nnat, <- S_INR.
-assert (tmp := epsilon_spec (inhabits nil) _ exv2 n' (S m') nnat m1nat).
-assert (tmp2 := epsilon_spec (inhabits nil) _ exv1 (S n') m' n1nat mnat).
+assert (tmp := epsilon_spec (inhabits nil) _ exv2 (S m') m1nat).
+assert (tmp2 := epsilon_spec (inhabits nil) _ exv1 m' mnat).
 unfold Rseq; rewrite tmp, tmp2; simpl.
-now rewrite <- INR_IZR_INZ, nnat.
+rewrite Rplus_0_r, <- seq_shift, map_map.
+apply f_equal; apply map_ext.
+intros a; rewrite Nat2Z.inj_succ, succ_IZR; ring.
 Qed.
 
+Lemma Rseq_S' (n m : R) : Rnat (m - 1) ->
+  Rseq n m = n :: Rseq (n + 1) (m - 1).
+Proof.
+intros mn; replace m with (m - 1 + 1) at 1 by ring.
+now rewrite Rseq_S; auto.
+Qed.
 
-(* This is the translation of theorem seq_app; most of the work is
-  automatic. *)
-Lemma Rseq_app (n m p : R) : Rnat n -> Rnat m -> Rnat p ->
+Lemma Rseq_app (n m p : R) : Rnat m -> Rnat p ->
   Rseq n (m + p) = (Rseq n m) ++ Rseq (n + m) p.
-intros [n' nnat] [m' mnat] [p' pnat].
-assert (exv1 : exists v, Rseq_Prop n (m + p) v).
-  exists (map (fun e => IZR (Z.of_nat e)) (seq n' (m' + p'))).
-  intros n'' mp''; rewrite nnat, mnat, pnat; intros nnat' mpnat'.
-  rewrite <- plus_INR in mpnat'.
-  now apply INR_eq in nnat', mpnat'; rewrite <- nnat', mpnat'.
-assert (exv2 : exists v, Rseq_Prop n m v).
-  exists (map (fun e => IZR (Z.of_nat e)) (seq n' m')).
-  intros n'' m''; rewrite nnat, mnat; intros nnat' mnat'.
-  now apply INR_eq in nnat', mnat'; rewrite <- nnat', mnat'.
-assert (exv3 : exists v, Rseq_Prop (n + m) p v).
-  exists (map (fun e => IZR (Z.of_nat e)) (seq (n' + m') p')).
-  intros n'' m''; rewrite nnat, mnat, pnat; intros nmnat' mnat'.
-  rewrite <- plus_INR in nmnat'.
-  now apply INR_eq in nmnat', mnat'; rewrite <- nmnat', mnat'.
-assert (mpnat : m + p = INR (m' + p')) by now rewrite mnat, pnat, plus_INR.
-assert (nmnat : n + m = INR (n' + m')) by now rewrite nnat, mnat, plus_INR.
-assert (tmp1 :=
-   epsilon_spec (inhabits nil) _ exv1 n' (m' + p')%nat nnat mpnat).
-assert (tmp2 :=
-   epsilon_spec (inhabits nil) _ exv2 n' m' nnat mnat).
-assert (tmp3 :=
-   epsilon_spec (inhabits nil) _ exv3 (n' + m')%nat p' nmnat pnat).
-now unfold Rseq; rewrite tmp1, tmp2, tmp3, seq_app, map_app.
+Proof.
+intros mnat pnat; revert n.
+induction mnat as [ | m' m'nat Ih] using Rnat_ind; intros n.
+  now rewrite Rseq0, Rplus_0_l, Rplus_0_r.
+rewrite Rseq_S';
+  replace (m' + 1 + p - 1) with (m' + p) by ring; auto.
+rewrite Rseq_S; simpl; auto; rewrite Ih.
+now rewrite <- (Rplus_comm 1 m'), <- Rplus_assoc.
 Qed.
 
 Lemma INR_IZN x : (0 <= x)%Z -> IZR x = INR (Z.to_nat x).
@@ -235,46 +228,33 @@ Qed.
 
 Ltac expand_Rseq :=
   match goal with
-  | |- context[Rseq (IZR ?x) (IZR ?y)] =>
+  | |- context[Rseq ?x (IZR ?y)] =>
     let Hex := fresh "existential_hypothesis" in
-    assert (Hex : exists v, Rseq_Prop (IZR x) (IZR y) v);
-    [exists (map (fun e => (IZR (Z.of_nat e))) (seq (Z.to_nat x) (Z.to_nat y)));
-     let x := fresh "x" in let y := fresh "y" in
-     intros x y; rewrite 2!INR_IZR_INZ;
-     let xeq := fresh "eq_for_x" in let yeq := fresh "eq_for_y" in
-     intros xeq yeq; apply eq_IZR in xeq; apply eq_IZR in yeq; rewrite xeq, yeq, !Nat2Z.id; reflexivity |
-  apply (epsilon_spec (inhabits nil) (Rseq_Prop (IZR x) (IZR y)) Hex (Z.to_nat x) (Z.to_nat y)); apply INR_IZN; lia]
+    assert (Hex : exists v, Rseq_Prop x (IZR y) v);
+    [exists (map (fun e => x + (IZR (Z.of_nat e))) (seq 0 (Z.to_nat y)));
+     let y := fresh "y" in
+     intros y; rewrite ?INR_IZR_INZ;
+     let yeq := fresh "eq_for_y" in
+     intros yeq; apply eq_IZR in yeq; rewrite yeq, !Nat2Z.id; reflexivity |
+  unfold Rseq;
+  rewrite (epsilon_spec (inhabits nil)
+             (Rseq_Prop x (IZR y)) Hex (Z.to_nat y));
+     [simpl; rewrite <- !plus_IZR; reflexivity | rewrite INR_IZN; auto; lia ]]
   end.
+
+Ltac expand_Rseq' :=
+  repeat (rewrite Rseq_S', <- minus_IZR;
+     [ | rewrite <- minus_IZR;
+        (apply Rnat_imm || apply Rnat0)]); rewrite Rseq0;
+        rewrite <- !plus_IZR; reflexivity.
 
 Example seq14 : Rseq 1 4 = [1; 2; 3; 4].
 Proof.
-expand_Rseq.
+expand_Rseq'.
+(* expand_Rseq. also works. *)
 Qed.
 
-Definition increase_list (l : list R) :=
-  match l with nil => 0::nil | a :: tl => (a + 1) :: l end.
-
-Example increase_list_4 : Rnat_iter 4 increase_list nil =
-   3 :: 2 :: 1 :: 0 :: nil.
-Proof.
-assert (Rnat3 : Rnat (0 + 1 + 1 + 1)).
-  exists 3%nat; compute; ring.
-assert (Rnat2 : Rnat (0 + 1 + 1)).
-  exists 2%nat; compute; ring.
-assert (Rnat1 : Rnat (0 + 1)).
-  exists 1%nat; compute; ring.
-assert (Rnat0 : Rnat 0).
-  now exists 0%nat.
-assert (four1 : 4 = 0 + 1 + 1 + 1 + 1) by ring.
-rewrite four1 at 1.
-repeat rewrite Rnat_iterS; auto; cycle 1.
-rewrite Rnat_iter0.
-simpl.
-replace (0 + 1 + 1 + 1) with 3 by ring.
-replace (0 + 1 + 1) with 2 by ring.
-replace (0 + 1) with 1 by ring.
-easy.
-Qed.
+(* We can use structural recursion on datatypes as usual. *)
 
 Fixpoint sumr (l : list R) : R :=
   match l with nil => 0 | a :: tl => a + sumr tl end.
@@ -297,153 +277,117 @@ rewrite Rseq_app; try (apply Rnat0 || apply Rnat1 || assumption).
 rewrite sumr_app.
 rewrite IHnnat.
 replace 1 with (0 + 1) at 2 by now rewrite Rplus_0_l.
-rewrite Rseq_S;[ | apply Rnat_add | ]; try apply Rnat0; auto.
+rewrite Rseq_S; auto.
 rewrite Rseq0; simpl.
   (*  field. can finish here. *)
-  rewrite Rplus_0_l.
-  rewrite Rplus_0_r.
-  unfold Rminus at 2.
-  rewrite Rplus_assoc.
-  rewrite Rplus_opp_r.
-  rewrite Rplus_0_r.
-  replace k with ((2 * k) / 2) at 3.
-    rewrite <- Rdiv_plus_distr.
-    rewrite (Rmult_comm k).
-    rewrite <- Rmult_plus_distr_r.
-    unfold Rminus; rewrite Rplus_assoc.
-    (* The next line needs more thinking. *)
-    replace (-(1) + 2) with 1 by ring.
-    reflexivity.
-  unfold Rdiv.
-  rewrite (Rmult_comm 2).
-  rewrite Rmult_assoc.
-  rewrite Rinv_r.
-    rewrite Rmult_1_r.
-    reflexivity.
+rewrite Rplus_0_l.
+rewrite Rplus_0_r.
+unfold Rminus at 2.
+rewrite Rplus_assoc.
+rewrite Rplus_opp_r.
+rewrite Rplus_0_r.
+replace k with ((2 * k) / 2) at 3.
+  rewrite <- Rdiv_plus_distr.
+  rewrite (Rmult_comm k).
+  rewrite <- Rmult_plus_distr_r.
+  unfold Rminus; rewrite Rplus_assoc.
+  (* The next line needs more thinking. *)
+  replace (-(1) + 2) with 1 by ring.
+  reflexivity.
+unfold Rdiv.
+rewrite (Rmult_comm 2).
+rewrite Rmult_assoc.
+rewrite Rinv_r.
+  rewrite Rmult_1_r.
+  reflexivity.
   (* The next line needs more thinking *)
-  lra.
-  (* end of the proof done by field. *)
-repeat apply Rnat_add; auto; apply Rnat0 || apply Rnat1.
-Qed.
-
-Definition rev_iota (n : R) := Rnat_iter n increase_list nil.
-
-Lemma riota_head n : Rnat n ->
-  hd (-1) (Rnat_iter n increase_list nil) = n - 1.
-Proof.
-intros nnat; induction nnat using Rnat_ind.
-  rewrite Rnat_iter0; simpl; ring.
-rewrite Rnat_iterS; auto.
-destruct (Rnat_iter k increase_list nil) as [ | b l].
-  simpl in IHnnat |- *; lra.
-simpl in IHnnat |- *; rewrite IHnnat; ring.
+lra.
 Qed.
 
 Fixpoint rlength {A : Type} (l : list A) : R :=
   match l with nil => 0 | a :: tl => rlength tl + 1 end.
 
-Lemma riota_length n : Rnat n ->
-  rlength (rev_iota n) = n.
-intros nnat; induction nnat using Rnat_ind.
-  now unfold rev_iota; rewrite Rnat_iter0; simpl.
-unfold rev_iota in IHnnat |- *.
-rewrite Rnat_iterS; auto; destruct (Rnat_iter k increase_list nil) as [ | a l].
-  simpl in IHnnat |- *.
-  now rewrite <- IHnnat.
-simpl in IHnnat |- *.
-now rewrite IHnnat.
-Qed.
-
 Definition rnth {A : Type} (e : A) (n : R) (l : list A) :=
   hd e (Rnat_iter n (@tl A) l).
 
-Lemma riota_nth (n p : R) : Rnat n -> Rnat p ->
-  p < n -> rnth 0 p (rev_iota n) = n - p - 1.
-Proof.
-intros nnat; revert p.
-induction nnat using Rnat_ind.
-  intros p pnat; unfold rnth; simpl.
-  intros plt0.
-  assert (pge0 := Rnat_ge0 p pnat); lra.
-intros p pnat pltk1.
-assert (plek : p <= k) by (apply Rnat_lt_succ; auto).
-case (Req_dec p 0).
-  intros p0; unfold rnth; rewrite p0, Rnat_iter0, Rminus_0_r.
-  assert (kp1gt0 : 0 < k + 1) by (generalize (Rnat_ge0 k nnat); lra).
-  assert (k1nat : Rnat (k + 1)) by (apply Rnat_add;[assumption | apply Rnat1]).
-  apply riota_head in k1nat.
-  fold (rev_iota (k + 1)) in k1nat.
-  destruct (rev_iota (k + 1)); simpl in k1nat |- *; lra.
-intros pn0.
-assert (pm1nat : Rnat (p - 1)).
-  apply Rnat_sub;[exact pnat | exact Rnat1 | ].
-  apply Rnat_lt_succ;
-   [exact Rnat1 | auto | assert (t := Rnat_ge0 p pnat); lra].
-assert (pm1ltk : p - 1 < k) by lra.
-assert (IH' := IHnnat _ pm1nat pm1ltk).
-replace p with ((p - 1) + 1) by ring.
-replace (rev_iota (k + 1)) with (k :: rev_iota k); cycle 1.
-  assert (t1 := riota_head k nnat).
-  unfold rev_iota; rewrite Rnat_iterS; auto.
-  destruct (Rnat_iter _ _ _) as [ | k' l].
-    simpl in t1.
-    assert (0 <= p - 1) by now apply Rnat_ge0.
-    lra.
-  simpl in t1 |- *; rewrite t1.
-  now replace (k - 1 + 1) with k by ring.
-replace (rnth 0 (p - 1 + 1) (k :: rev_iota k)) with
-  (rnth 0 (p - 1) (rev_iota k)).
-  rewrite IH'; ring.
-unfold rnth.
-now rewrite Rnat_iter_add, Rnat_iter1; auto.
-Qed.
-
-Lemma increase_listP n : Rnat n ->
-  sumr (Rnat_iter n increase_list nil) = n * (n - 1) / 2.
-Proof.
-intros nnat; induction nnat using Rnat_ind.
-rewrite Rnat_iter0; simpl; field.
-rewrite Rnat_iterS; simpl; auto.
-generalize (riota_head k nnat).
-destruct (Rnat_iter k increase_list nil) as [ | k' l].
-  simpl; intros k0.
-  assert (k0' : 0 = k) by lra.
-  rewrite <- k0'; field.
-simpl in IHnnat |- *.
-rewrite IHnnat.
-intros k'k; rewrite k'k; field.
-Qed.
-
-Fact Rseq_example : Rseq 0 5 = [0; 1; 2; 3; 4].
-Proof.
-assert (nat0 : Rnat 0) by now exists 0%nat.
-assert (nat4 : Rnat 4) by now exists 4%nat; simpl; ring.
-change 5%Z with (((((0 + 1) + 1) + 1)+ 1) + 1)%Z;
- rewrite !plus_IZR, !Rseq_S, Rseq0;
- repeat (apply Rnat_add || apply Rnat1 || apply Rnat0).
-rewrite <- ! plus_IZR; simpl.
-reflexivity.
-Qed.
-
 Notation "'\big[' f / idf ]_( a <= i < b ) E" :=
-  (fold_left (fun v i => f v E) (Rseq a b) idf)
+  (fold_right f  idf (map (fun i => E) (Rseq a (b - a))))
   (at level 35, a at level 30,  b at level 30, E at level 36, f,
    idf at level 10, i at level 0, right associativity).
 
-Lemma Rseq1 n : Rnat n -> Rseq n 1 = [n].
+Lemma Rseq1 n : Rseq n 1 = [n].
 Proof.
-intros nn; replace 1 with (0 + 1) at 1 by ring.
-rewrite Rseq_S; auto; rewrite Rseq0; auto.
-now apply Rnat_add; auto.
+replace 1 with (0 + 1) at 1 by ring.
+now rewrite Rseq_S; auto; rewrite Rseq0; auto.
 Qed.
+
+Definition associative_monoid {A : Type} (f : A -> A -> A) (idx : A) :=
+  (forall x y z, f x (f y z) = f (f x y) z) /\
+   (forall x, f x idx = x) /\
+   (forall x, f idx x = x).
+
+Lemma big0 {A : Type}(E : R -> A) (f : A -> A -> A) (idx : A) (a : R) :
+  \big[f / idx]_(a <= i < a) E i = idx.
+Proof.
+now rewrite Rminus_eq_0, Rseq0.
+Qed.
+
+Lemma big_recl {A : Type}(E : R -> A) (f : A -> A -> A) (idx : A) (a b : R) :
+  Rnat (b - a) -> a < b ->
+  \big[f / idx]_(a <= i < b) E i =
+   f (E a) (\big[f / idx]_((a + 1) <= i < b) E i).
+Proof.
+intros hnat altb.
+rewrite Rseq_S'; [ | apply Rnat_sub; auto]; simpl.
+  replace (b - a - 1) with (b - (a + 1)) by ring.
+  easy.
+assert (0 < b - a) by lra.
+apply Rnat_gt_pred; auto; lra.
+Qed.
+
+Lemma big_recr {A : Type}(E : R -> A) (f : A -> A -> A) (idx : A) (a b : R) :
+  associative_monoid f idx ->
+  Rnat (b - a) -> a < b ->
+  \big[f / idx]_(a <= i < b) E i =
+   f (\big[f / idx]_(a <= i < (b - 1)) E i)
+    (E (b - 1)).
+Proof.
+intros amf hnat altb.
+assert (induct_arg : Rnat (b - a  - 1)).
+  apply Rnat_sub; auto; apply Rnat_gt_pred; auto; lra.
+enough (main : forall p, Rnat p ->
+  forall a, fold_right f idx (map (fun i => E i) (Rseq a (p + 1))) =
+   f (fold_right f idx (map (fun i => E i) (Rseq a p))) (E (a + p))).
+  replace (b - a) with (b - a - 1 + 1) by ring.
+  rewrite main; auto.
+  replace (a + (b - a - 1)) with (b - 1) by ring.
+  now replace (b - 1 - a) with (b - a - 1) by ring.
+clear hnat altb induct_arg a.
+intros p'; induction 1 as [ | p pnat Ih] using Rnat_ind; clear p'.
+  intros a; rewrite Rplus_0_l, Rplus_0_r, Rseq0, Rseq1; simpl.
+  now destruct amf as [_ [P1 P2]]; rewrite P1, P2.
+intros a; rewrite Rseq_S; auto; simpl.
+rewrite (Rseq_S a); auto; simpl.
+destruct amf as [Pa [P1 P2]].
+now rewrite Ih, Pa; replace (a + (p + 1)) with (a + 1 + p) by ring.
+Qed.
+
+Lemma associative_monoid_Rplus : associative_monoid Rplus 0.
+Proof.
+split;[exact (fun x y z => eq_sym (Rplus_assoc x y z))| ].
+split;[exact Rplus_0_r | exact Rplus_0_l].
+Qed.
+
+Hint Resolve associative_monoid_Rplus : core.
 
 Lemma sumr_again n :
   Rnat n -> \big[Rplus / 0]_(0 <= i < n) i = n * (n - 1) / 2.
 Proof.
 induction 1 as [ | p Np Ih] using Rnat_ind.
-  rewrite Rseq0; [now simpl; unfold Rdiv; rewrite !Rmult_0_l | auto].
-rewrite Rseq_app; auto.
-rewrite Rseq1; auto;[ | now apply Rnat_add; auto].
-rewrite fold_left_app; simpl; rewrite Ih; clear Ih.
-field.
+  rewrite big0; field.
+assert (Rnat (p + 1 - 0)) by now rewrite Rminus_0_r; auto.
+assert (0 < p + 1) by now assert (tmp := Rnat_ge0 _ Np); lra.
+rewrite big_recr; auto.
+replace (p + 1 - 1) with p by ring.
+rewrite Ih; field.
 Qed.
