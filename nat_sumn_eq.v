19713c52fd5c7734cbc9edef1f8403bf5fcb3db6
Yves Bertot <Yves.Bertot@inria.fr>
2024-05-30 08:16:07 +0200

    adds a few comments to the file.  This file is actually not part of the solution, but part of illustrating the problem.

--- nat_sumn_eq.v
+++ nat_sumn_eq.v
@@ -1,8 +1,13 @@
 Require Import Arith Lia.
 
+(* This file is rather a counter-example showing the difficulty of working
+  with natural numbers and euclidean division. *)
+
 Fixpoint sumn (n : nat) :=
   match n with S n' => sumn n' + n' | 0 => 0 end.
 
+(* This shows the difficulty of reasoning about the well-known theorem
+  concerning the first n integers, where division by 2 makes life difficult. *)
 Lemma sumn_eq (n : nat) : sumn n = (n * (n - 1)) / 2.
 Proof.
 induction n as [ | p Ih].
@@ -21,7 +26,10 @@ rewrite <- (Nat.mul_cancel_r _ _ 2); auto.
 Search ((_ + _) * _).
 rewrite Nat.mul_add_distr_r.
 Search (_ * (_ / _)).
+(* At this point, we wish to get rig of division by 2.  We know that
+  S k * k is even, because at least one of the two factors is even. *)
 assert (even_prod : forall k, S k * k / 2 * 2 = S k * k).
+  (* Here is where we say that the product is even. *)
   assert (aux : forall k, exists l, S k * k = l * 2).
     intros k; induction k as [ | k' Ihk].
       exists 0; easy.
