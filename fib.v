fcc07848204fbac9a7f5967c688a5b019b67f9ca
Yves Bertot <Yves.Bertot@inria.fr>
2024-06-25 14:45:23 +0200

    adapts fib.v to Rnat_rec without membership condition

--- fib.v
+++ fib.v
@@ -9,21 +9,20 @@ Inductive Rnat : R -> Prop :=
 | Rnat_succ : forall n, Rnat n -> Rnat (n + 1).
 
 Parameter Rnat_rec :
-  forall {A : Type} (v0 : A) (step : R -> A -> A) (n : R)
-    (h : Rnat n), A.
+  forall {A : Type} (v0 : A) (step : R -> A -> A), R -> A.
 
 Parameter Rnat_rec0 : forall {A : Type} (v0 : A) (step : R -> A -> A),
-  Rnat_rec v0 step 0 Rnat0 = v0.
+  Rnat_rec v0 step 0 = v0.
 
 Parameter Rnat_rec_succ : forall {A : Type} (v0 : A) (step : R -> A -> A)
-  n (h : Rnat n) (h' : Rnat (n + 1)),
-  Rnat_rec v0 step (n + 1) h' = step n (Rnat_rec v0 step n h).
+  n, Rnat n ->
+  Rnat_rec v0 step (n + 1) = step n (Rnat_rec v0 step n).
 
 Definition fib' :=
-  fun x (h : Rnat x) =>
+  fun x =>
   nth 0 (Rnat_rec (0 :: 1 :: nil)
           (fun n v => (nth 1 v 0 :: nth 0 v 0 + nth 1 v 0 :: nil))
-          x h)
+          x)
     0.
 
 Lemma RnatZ x: Rnat (IZR (Z.pos x)).
@@ -36,18 +35,16 @@ rewrite Nat2Z.inj_succ, succ_IZR.
 now apply Rnat_succ, IHn.
 Qed.
 
-(* This proof is reminder that functions with domain definition conditions
-  are a pain to use.  The rewrite rules are just not helping. *)
-Lemma test_fib3 : fib' 3 (RnatZ 3) = 2.
+(* With Rnat_rec that is free of membership condition, it is comfortable
+  to compute with the rewrite rules, with the minor nuisance of checking
+  whether the membership condition holds only when triggering the rewrite
+  rule. *)
+Lemma test_fib3 : fib' 3 = 2.
 Proof.
 unfold fib'.
-generalize (RnatZ 3); replace 3 with (0 + 1 + 1 + 1) by ring.
-intros r.
-assert (r' : Rnat (0 + 1 + 1)) by now repeat apply Rnat_succ; apply Rnat0.
-rewrite Rnat_rec_succ with (h := r'); simpl.
-assert (r'' : Rnat (0 + 1)) by now repeat apply Rnat_succ; apply Rnat0.
-rewrite Rnat_rec_succ with (h := r''); simpl.
-rewrite Rnat_rec_succ with (h := Rnat0); simpl.
-rewrite Rnat_rec0; simpl.
-ring.
+replace 3 with (0 + 1 + 1 + 1) by ring.
+ do 3 (try (rewrite Rnat_rec_succ; simpl)).
+ rewrite Rnat_rec0; simpl.
+ ring.
+ all: repeat apply Rnat_succ; apply Rnat0.
 Qed.
