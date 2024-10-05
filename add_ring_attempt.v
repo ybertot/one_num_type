cf44e6a7d42173cad8938fd5d220778e977892b5
Yves Bertot (he) <yves.bertot@inria.fr>
2024-10-05 17:56:06 +0200

    I found how to make Rpow the power function for ring, in a way that is satisfactory in the presentation of the numbers.

--- add_ring_attempt.v
+++ add_ring_attempt.v
@@ -1,4 +1,5 @@
 Require Import Reals.
+Require Import Field.
 
 Open Scope R_scope.
 
@@ -16,11 +17,13 @@ easy.
 Qed.
 
 (* This is a sub-part of the field declaration used for
-  the real numbers.  Normally, it should only remove the
-  handling of powers.  This field declaration is inspired from
-  the last lines in the sources of Coq in file
-  theories/setoid_ring/RealField.v
-  *)
+  the real numbers.  It illustrates the need to declare
+  morphisms using the morphisms modifier.   When
+  no morphism are provided, all integer constants
+  are expressed solely using the unit of the ring,
+  addition, and multiplication, using a binary number
+  structure.
+*)
 Add Field RField2 : Rfield
  (completeness Zeq_bool_complete, constants [IZR_tac]).
 
@@ -34,7 +37,8 @@ syntactic equality with the expansion of IZR 5. *)
 easy.
 Qed.
 
-(* As expected, ring_simplify does not produce anything. *)
+(* As expected, ring_simplify does not produce anything.  Because
+  the ring description used here has no knowldge of the power function. *)
 Example test2 (n : R) : n ^ 3 = n * n * n.
 Proof.
 Fail progress ring_simplify.
@@ -43,6 +47,11 @@ ring_simplify.
 easy.
 Qed.
 
+(* The previous experiments highlights what happens when the
+  IZR functio is expanded.  Unfortunately, if we want IZR to appear
+  as part of the exponent argument in powers, it will be expanded.
+  The following Module introduces a copy of the IZR function that
+  will not be expanded, but is provably equal to the known IZR.  *)
 Module Type MyIZR_type.
 
 Parameter MyIZR : Z -> R.
@@ -60,20 +69,27 @@ Proof. reflexivity. Qed.
 
 End MyIZR.
 
+(* The Ring tactic uses the type N to encode the exponents
+  of powers, and if one uses a power function with a different
+  type for the exponents, one must provide a function to translate
+  the elements of type N to the used type.  In our case, we aim
+  to use the function Rpow defined in R_subsets.v, where exponents
+  are in type R.   We define a function mapping N to R. *)
 Definition MyINR : N -> R :=
 fun n => match n with
 | 0%N => 0
 | N.pos p => IZR (Z.pos p)
-  (* Alternatively: MyIZR.MyIZR instead of IZR*)
 end.
 
 Definition MyINR1 : N -> R :=
 fun n => match n with
 | 0%N => 0
 | N.pos p => MyIZR.MyIZR (Z.pos p)
-  (* Alternatively: MyIZR.MyIZR instead of IZR*)
 end.
 
+(* For the experiments in this file, we do no load the file
+  R_subset, but assume the existence of the relevant function,
+  with two axioms describing the expected properties. *)
  Parameter Rpow : R -> R -> R.
 
  Axiom Rpow_convert0 : forall n, Rpow n 0 = 1.
@@ -81,21 +97,28 @@ end.
  Axiom Rpow_convertp : forall n p, Rpow n (IZR (Z.pos p)) =
    pow n (Pos.to_nat p).
 
+(* the following structure uses MyINR, where the IZR
+   function is used, and we will see in experiments that
+   the exponents produced by ring_simplify are uncomfortable. *)
 Definition R_p_t : power_theory 1 Rmult (@eq R)
   MyINR Rpow.
 constructor.
 destruct n.
   simpl.
   now rewrite Rpow_convert0.
-(* The option rewrite in the next line is to be robust to the
+(* The optional rewrite in the next line is to be robust to the
   fact that MyINR may call directly IZR or MyIZR.MyIZR instead. *)
 unfold MyINR; rewrite ?MyIZR.MyIZR_eq.
-simpl (RMicromega.INZ (N.pos p)).
 rewrite Rpow_convertp.
 change (Pos.to_nat p) with (N.to_nat (N.pos p)).
+(* We use the fact that the correctness property is already proved
+  in an existing structure. *)
 now destruct R_power_theory as [ it]; apply it.
 Qed.
 
+(* The following structure use MyINR1, where the MyIZR function
+  is used instead.  We will see that it makes the results of
+  ring_simplify more readable, but still slightly uncomfortable. *)
 Definition R_p_t1 : power_theory 1 Rmult (@eq R)
   MyINR1 Rpow.
 constructor.
@@ -108,9 +131,14 @@ unfold MyINR1; rewrite ?MyIZR.MyIZR_eq.
 simpl (RMicromega.INZ (N.pos p)).
 rewrite Rpow_convertp.
 change (Pos.to_nat p) with (N.to_nat (N.pos p)).
+(* We use the fact that the correctness property is already proved
+  in an existing structure. *)
 now destruct R_power_theory as [ it]; apply it.
 Qed.
 
+(* This Ltac function performs the inverse of MyINR,  mapping
+  any real value that could have been the result of applying  MyINR
+  to a constant of type N back to that constant. *)
 Ltac Rpow_tac1 t :=
   match t with
   | IZR Z0 => N0
@@ -133,7 +161,9 @@ Add Field RField3 : Rfield
 Example test2_1 : 3 + 2 = 5.
 Proof. ring_simplify (3 + 2). easy. Qed.
 
-(* As expected, ring_simplify does not produce *)
+(* As expected, the exponents produced in power expressins are not
+  protected agains the computation of IZR, so that the formula we obtain
+  in uncomfortable to read. *)
 Example test2_2 (n : R) : Rpow n 10 = n * n * n * n * n * n * n * n * n * n.
 Proof.
 (* ring succeeds in handling powers, but the exponent is unsightly,
@@ -152,21 +182,27 @@ Example test3_1 : 3 + 2 = 5.
 Proof. ring_simplify (3 + 2). easy. Qed.
 
 (* As expected, ring_simplify does not produce *)
-Example test3_2 (n : R) : Rpow n 3 = n * n * n.
+Example test3_2 (n : R) : Rpow n 10 = n * n * n * n * n * n * n * n * n * n.
 Proof.
 (* ring succeeds in handling powers, but the exponent is relying on MyIZR,
-  with blocked computation. *)
-ring_simplify.
+  with blocked computation. This is readable, but final users will wonder what
+  this MyIZR.MyIZR function is. *)
+field_simplify.
 rewrite MyIZR.MyIZR_eq.
 easy.
 Qed.
 
-Ltac r_simplify_post := rewrite 1?MyIZR.MyIZR_eq.
-
+(* We can request that ring_simplify (or field_simplify) performs systematically
+  the rewrite shown above.*)
 Add Field RField5 : Rfield
-  (completeness Zeq_bool_IZR, morphism R_rm, constants [IZR_tac], 
-    power_tac R_p_t1 [Rpow_tac1], postprocess [r_simplify_post]).
+  (completeness Zeq_bool_IZR, morphism R_rm, constants [IZR_tac],
+    postprocess [rewrite 1?MyIZR.MyIZR_eq], power_tac R_p_t1 [Rpow_tac1]).
 
+(* It is necessary to perform the Add Ring command hereafter,
+  because Add Field adds a ring declaration for R, but with
+  no postprocess tactic. *)
+Add Ring RRing5 : RTheory (morphism R_rm, constants[IZR_tac],
+  postprocess [rewrite 1?MyIZR.MyIZR_eq], power_tac R_p_t1 [Rpow_tac1]).
 
 (* Testing the simplification of ground expressions containing
   integers. We recover the correct handling of integers. *)
@@ -176,13 +212,11 @@ Proof. ring_simplify (3 + 2). easy. Qed.
 (* As expected, ring_simplify does not produce *)
 Example test4_2 (n : R) : Rpow n 10 = n * n * n * n * n * n * n * n * n * n.
 Proof.
-(* I expected the results of r_simplify to contain no occurrence
-  of MyIZR.MyIZR, since they should have been rewritten.  This
-  feels like a bug.  Unfortunately, there is no use of the
-  postprocess directive in the Coq sources. *)
+(* The results of ring_simplify are now comfortable for the final user,
+  who does not need to see (and to know) that there is an IZR function
+  (here used as hidden coercion) in the display of integer constants of
+  type R. *)
 ring_simplify.
-(* The next line should be unnecessary. *)
-rewrite MyIZR.MyIZR_eq.
 easy.
 Qed.
 
