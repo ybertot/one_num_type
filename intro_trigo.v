7d6e17df344e9dedda993229f01c92e779d40193
Yves Bertot <Yves.Bertot@inria.fr>
2025-07-21 17:03:19 +0200

    a little more trigonometry

--- intro_trigo.v
+++ intro_trigo.v
@@ -17,6 +17,11 @@ Parameters cos sin : R -> R.
 Axiom par_sin : forall x, sin (- x) = - (sin x).
 Axiom par_cons : forall x, cos (- x) = cos x.
 
+Axiom unit_circle : forall x, cos x ^ 2 + sin x ^ 2 = 1.
+
+Axiom cos_add : forall x y, cos (x + y) = cos x * cos y - sin x * sin y.
+Axiom sin_add : forall x y, sin (x + y) = cos x * sin y + cos y * sin x.
+
 Axiom derivable_cos : forall x, derivable cos x.
 Axiom derive_cos : forall x, derive cos x = -sin x.
 Axiom derivable_sin : forall x, derivable sin x.
@@ -33,20 +38,35 @@ Ltac end_calculate :=
    match goal with | id : _ = _ |- _ => rewrite id; clear id end;
    easy.
 
-Ltac assert_LHS F  :=
+Ltac calc_LHS F  :=
   match goal with
   | |- ?L = _ =>
-    let name := fresh "temp_for_assert_LHS" in
-     assert (name: L = F);[ | rewrite name; clear name]
+    let name := fresh "temp_for_calc_LHS" in
+     assert (name: L = F);[ | apply (eq_trans name); clear name]
   end.
 
 Lemma sin0 : sin 0 = 0.
 Proof.
-assert_LHS ((sin 0 + sin 0) / 2).
+calc_LHS ((sin 0 + sin 0) / 2).
   now field.
-assert_LHS ((sin (-0) + sin 0) / 2).
+calc_LHS ((sin (-0) + sin 0) / 2).
   now replace (-0) with 0 by ring.
-assert_LHS ((- sin 0 + sin 0) / 2).
+calc_LHS ((- sin 0 + sin 0) / 2).
   now rewrite par_sin.
 now field.
+Qed.
+
+Lemma cos0 : cos 0 = 1.
+Proof.
+calc_LHS (cos (0 + 0)).
+  now replace (0 + 0) with 0 by ring.
+calc_LHS (cos 0 * cos 0 - sin 0 * sin 0).
+  now rewrite cos_add.
+calc_LHS (cos 0 * cos 0 - 0 * 0).
+  now rewrite sin0.
+calc_LHS (cos 0 * cos 0).
+  now ring.
+calc_LHS (cos 0  ^ 2 + sin 0 ^ 2).
+  now rewrite sin0; ring.
+apply unit_circle.
 Qed.
