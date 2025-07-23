4fbc7e644b467e6f21ea7837c2d51f9e8891a2e9
Yves Bertot <Yves.Bertot@inria.fr>
2025-07-23 15:25:52 +0200

    a presentation of the sine and cosine functions, based on initial properties which are considered as axioms.
    The code performs quite a good amount of computation that could be viewed
    as instance of the 'calculate' mode where one chains equalities, interspersed
    with justifications

--- intro_trigo.v
+++ intro_trigo.v
@@ -3,7 +3,7 @@ From Stdlib Require Import Reals ZArith Lra.
 Open Scope R_scope.
 
 Ltac end_calculate :=
-  repeat 
+  repeat
    match goal with | id : _ = _ |- _ => (rewrite id || rewrite <- id); clear id end;
    (easy || ring || (field; (easy || lra))).
 
@@ -35,6 +35,13 @@ assert (x < 0 \/ 0 < x) as [xneg | xpos] by lra.
   apply pow_lt; lra.
 apply pow_lt; lra.
 Qed.
+
+(* We believe that in a mathematical class, we cannot assume sqrt to be defined
+  for all real number, we should rather respect that the value is well defined
+  only for positive real numbers. sqrt_pos should not be used in our context,
+  but sqrt_pos' instead. *)
+Lemma sqrt_pos' x : 0 <= x -> 0 <= sqrt x.
+Proof. intros; apply sqrt_pos. Qed.
 
 End lemmas_that_should_exist.
 
@@ -75,7 +82,7 @@ End simple_trigo.
 
 Lemma trinom : forall a b c x, a <> 0 ->
   0 <= b ^ 2 - 4 * a * c ->
-  a * x ^ 2 + b * x + c = 0 -> 
+  a * x ^ 2 + b * x + c = 0 ->
     x = ((- b + sqrt (b ^ 2 - 4 * a * c)) / (2 * a)) \/
     x = ((- b - sqrt (b ^ 2 - 4 * a * c)) / (2 * a)).
 Proof.
@@ -127,7 +134,7 @@ assert (0 < 4 * a ^ 2).
 assert ( a * x ^ 2 + b * x + c = a * ( x ^ 2 + b / a * x + c / a)).
   field.
   lra.
-assert (x ^ 2 + b / a * x + c / a = 
+assert (x ^ 2 + b / a * x + c / a =
             (x + b / (2 * a)) ^ 2 + (c / a - b ^ 2 / (4 * a ^ 2))).
   field.
   lra.
@@ -351,7 +358,7 @@ calc_LHS (0 - 1 * 1).
   now rewrite sin_Pi_half.
 ring.
 Qed.
- 
+
 Lemma sin_add_Pi_half x : sin (x + Pi / 2) = cos x.
 Proof.
 start_with (sin (x + Pi / 2)).
@@ -486,6 +493,18 @@ calc_LHS (0 * cos x + 1 * sin x).
 ring.
 Qed.
 
+Lemma sin_Pi_half_sub x : sin (Pi / 2 - x) = cos x.
+Proof.
+start_with (sin (Pi / 2 - x)).
+calc_LHS (cos x * sin (Pi / 2) - cos (Pi / 2) * sin x).
+  now rewrite sin_sub.
+calc_LHS (cos x * 1 - cos (Pi / 2) * sin x).
+  now rewrite sin_Pi_half.
+calc_LHS (cos x * 1 - 0 * sin x).
+  now rewrite cos_Pi_half.
+ring.
+Qed.
+
 Lemma cos_double_1 x : cos (2 * x) = 2 * cos x ^ 2 - 1.
 Proof.
 assert (step : sin x ^ 2 = 1 - cos x ^ 2).
@@ -540,4 +559,116 @@ assert (0 < cos (Pi / 3)).
   apply first_cos_root.
   assert (tmp := Pi_gt0); lra.
 lra.
+Qed.
+
+Lemma sin_Pi_third : sin (Pi / 3) = sqrt 3 / 2.
+Proof.
+assert (1 * sin (Pi / 3) ^ 2 + 0 * sin (Pi / 3) + (- 3 / 4) = 0).
+  start_with (1 * sin (Pi / 3) ^ 2 + 0 * sin (Pi / 3) + (- 3 / 4)).
+  calc_LHS (sin (Pi / 3) ^ 2 - 1 + 1 / 4).
+    field.
+  calc_LHS (sin (Pi / 3) ^ 2 - (cos (Pi / 3) ^ 2 + sin (Pi / 3) ^ 2) + 1 /4).
+    now rewrite unit_circle.
+  calc_LHS (- (cos (Pi / 3) ^ 2) + 1 / 4).
+    ring.
+  calc_LHS (- (1 / 2) ^ 2 + 1 / 4).
+    now rewrite cos_Pi_third.
+  field.
+assert (0 <= 0 ^ 2 - 4 * 1 * (-3 / 4)).
+  lra.
+assert (0 < sin (Pi / 3)).
+  replace (sin (Pi / 3)) with (cos (Pi / 6)); cycle 1.
+    symmetry; start_with (sin (Pi / 3)).
+    calc_LHS (sin (Pi / 2 - Pi / 6)).
+      now replace (Pi / 2 - Pi / 6) with (Pi / 3) by field.
+    now rewrite sin_Pi_half_sub.
+  apply first_cos_root.
+  assert (tmp:= Pi_gt0); lra.
+assert (sin (Pi / 3) = (- 0 + sqrt (0 ^ 2 - 4 * 1 * (-3 / 4))) / (2 * 1) \/
+        sin (Pi / 3) = (- 0 - sqrt (0 ^ 2 - 4 * 1 * (-3 / 4))) / (2 * 1))
+  as [higher | lower].
+    apply trinom.
+        lra.
+      lra.
+    easy.
+  rewrite higher.
+  replace (0 ^ 2 - 4 * 1 * (-3 / 4)) with 3 by field.
+  field.
+enough (0 <= sqrt (0 ^ 2 - 4 * 1 * (-3 / 4))) by lra.
+now apply sqrt_pos'.
+Qed.
+
+
+Lemma cos_Pi_sixth : cos (Pi / 6) = sqrt 3 / 2.
+Proof.
+start_with (cos (Pi / 6)).
+calc_LHS (cos (Pi / 2 - Pi / 3)).
+  now replace (Pi / 2 - Pi / 3) with (Pi / 6) by field.
+calc_LHS (sin (Pi / 3)).
+  now rewrite cos_Pi_half_sub.
+now rewrite sin_Pi_third.
+Qed.
+
+Lemma sin_Pi_sixth : sin (Pi / 6) = 1 / 2.
+Proof.
+start_with (sin (Pi / 6)).
+calc_LHS (sin (Pi / 2 - Pi / 3)).
+  now replace (Pi / 2 - Pi / 3) with (Pi / 6) by field.
+calc_LHS (cos (Pi / 3)).
+  now rewrite sin_Pi_half_sub.
+now rewrite cos_Pi_third.
+Qed.
+
+Lemma common_sqrt_2 x : 0 < x -> 2 * x ^ 2 - 1 = 0 -> x = sqrt 2 / 2.
+Proof.
+intros xgt0 equa.
+assert (2 * x ^ 2 + 0 * x + (-1) = 0) by lra.
+(* It would be great to avoid an unreadable error message here. *)
+Fail (assert (x = (- 0 - sqrt (0 ^ 2 - 4 * 2 * (-1))) / (2 * 2) \/
+        x = (- 0 + sqrt (0 ^ 2 - 4 * 2 * (-1))) / (2 * 2)) as [higher | lower];
+  [apply trinom | | ]).
+assert (x = (- 0 + sqrt (0 ^ 2 - 4 * 2 * (-1))) / (2 * 2) \/
+        x = (- 0 - sqrt (0 ^ 2 - 4 * 2 * (-1))) / (2 * 2)) as [higher | lower].
+    apply trinom.
+        lra.
+      lra.
+    easy.
+  rewrite higher.
+  start_with ((- 0 + sqrt (0 ^ 2 - 4 * 2 * (- 1))) / (2 * 2)).
+  calc_LHS ((sqrt (0 ^ 2 - 4 * 2 * (- 1))) / 4).
+    field.
+  calc_LHS ((sqrt (2 * 4)) / 4).
+    now replace (0 ^ 2 - 4 * 2 * -1) with (2 * 4) by ring.
+  calc_LHS ((sqrt 2 * sqrt 4) / 4).
+    now rewrite sqrt_mult; lra.
+  calc_LHS (sqrt 2 * 2 / 4).
+    now replace (sqrt 4) with 2 by now compute_sqrt.
+  field.
+assert (0 <= sqrt (0 ^ 2 - 4 * 2 * -1)) by now apply sqrt_pos'; lra.
+lra.
+Qed.
+
+Lemma cos_Pi_fourth : cos (Pi / 4) = sqrt 2 / 2.
+Proof.
+assert (2 * cos (Pi / 4) ^ 2 - 1 = 0).
+  start_with (2 * cos (Pi / 4) ^ 2 - 1).
+  calc_LHS (cos (2 * (Pi / 4))).
+    now rewrite <- cos_double_1.
+  calc_LHS (cos (Pi / 2)).
+    now replace (2 * (Pi / 4)) with (Pi / 2) by field.
+  now rewrite cos_Pi_half.
+assert (0 < cos (Pi / 4)).
+  apply first_cos_root.
+  assert (tmp := Pi_gt0); lra.
+now apply common_sqrt_2.
+Qed.
+
+Lemma sin_Pi_fourth : sin (Pi / 4) = sqrt 2 / 2.
+Proof.
+start_with (sin (Pi / 4)).
+calc_LHS (sin (Pi / 2 - Pi / 4)).
+  now replace (Pi / 2 - Pi / 4) with (Pi / 4) by field.
+calc_LHS (cos (Pi / 4)).
+  now rewrite sin_Pi_half_sub.
+now rewrite cos_Pi_fourth.
 Qed.
