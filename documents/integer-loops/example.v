c8062b8c59e151f036af7e638a0f5e2638f4c293
Yves Bertot <yves.bertot@inria.fr>
2025-11-19 08:54:27 +0100

    a version with a 'strong' tactic used for most obligations

--- documents/integer-loops/example.v
+++ documents/integer-loops/example.v
@@ -4,6 +4,8 @@ Open Scope Z_scope.
 
  Ltac Zify.zify_post_hook ::= Z.div_mod_to_equations.
 
+ Ltac strong := try (easy || ring || lia).
+
 Definition sum_n (n : Z) :=
   Z.iter n (fun '(s, i) => (s + i, i + 1)) (0, 0).
 
@@ -12,15 +14,21 @@ Lemma sum_n'_0 : sum_n' 0 = 0.
 easy.
 Qed.
 
+Module missing_lemmas_for_Z_iter.
+
+Lemma Z_0_le_ind :
+  forall P : Z -> Prop,
+  (forall n, (forall l, 0 <= l < n -> P l) -> P n) ->
+  forall n, P n.
+Proof. exact (well_founded_ind (Z.lt_wf 0)). Qed.
+
 Lemma Z_iter_succ_l : forall (i : Z) (A : Type) (f : A -> A) (a : A),
   0 <= i -> Z.iter (i + 1) f a = f (Z.iter i f a).
 Proof.
 intros i A f a xge0.
-Search Z.iter.
-rewrite iter_nat_of_Z;[ | lia].
-replace (Z.abs_nat (i + 1)) with (S (Z.abs_nat i)) by lia.
-simpl; rewrite <- iter_nat_of_Z;[ | lia].
-easy.
+rewrite iter_nat_of_Z; strong.
+replace (Z.abs_nat (i + 1)) with (S (Z.abs_nat i)) by strong.
+simpl; rewrite <- iter_nat_of_Z; strong.
 Qed.
 
 Lemma Z_iter_ind : forall {A : Type}, forall P : Z -> A -> Prop,
@@ -30,74 +38,77 @@ P 0 a ->
 forall n, 0 <= n -> P n (Z.iter n f a).
 Proof.
 intros A P a f pstart pstep.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+induction n as [n Ih] using Z_0_le_ind.
 intros nge0.
-destruct (n =? 0) eqn:cmp0.
-  replace n with 0 by lia.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0.
   exact pstart.
-replace n with ((n - 1) + 1) by ring.
-rewrite Z_iter_succ_l; [ | lia].
-apply pstep;[lia | ].
-apply Ih; lia.
+replace n with ((n - 1) + 1) by strong.
+rewrite Z_iter_succ_l; strong.
+apply pstep; strong.
+apply Ih; strong.
 Qed.
 
 Lemma Z_iter_succ_r : forall (i : Z) (A : Type) (f : A -> A) (a : A),
   0 <= i -> Z.iter (i + 1) f a = Z.iter i f (f a).
 Proof.
 intros i A f.
-induction i as [i Ih] using (well_founded_ind (Z.lt_wf 0)).
-intros a.
-destruct (i =? 0) eqn:cmp0.
-  intros ige0; replace i with 0 by lia.
-  easy.
-intros ige0.
-rewrite Z_iter_succ_l;[ | lia].
-replace i with ((i - 1) + 1) by ring.
-  rewrite Ih, Z_iter_succ_l;[easy | ..]; lia.
+induction i as [i Ih] using Z_0_le_ind.
+intros a ige0.
+assert (i = 0 \/ 0 < i) as [i0 | igt0] by strong.
+  replace i with 0; strong.
+rewrite Z_iter_succ_l; strong.
+replace i with ((i - 1) + 1) by strong.
+  rewrite Ih; strong.
+  rewrite Z_iter_succ_l; strong.
 Qed.
 
-Lemma sum_n'_formula n : 0 <= n ->
-  sum_n' n = (n * (n - 1)) / 2.
+End missing_lemmas_for_Z_iter.
+
+Import missing_lemmas_for_Z_iter.
+
+Lemma sum_n'_formula n : 0 <= n ->  sum_n' n = (n * (n - 1)) / 2.
 Proof.
 intros nge0; unfold sum_n', sum_n.
 set (P := fun i '(s, j) => i = j /\ s = (i * (i - 1) / 2)).
-assert (pstart : P 0 (0, 0)).
-  unfold P; lia.
-assert (pstep : forall k t, 0 <= k -> P k t ->
-  P (k + 1) ((fun '(s, j) => (s + j, j + 1)) t)).
-  intros k [s j] kge0 [kj sq].
-  split.
-    now rewrite kj.
-  lia.
-generalize (Z_iter_ind P (0, 0) (fun '(s, j) => (s + j, j + 1)) pstart
-             pstep n nge0).
+assert (main : P n (Z.iter n (fun '(s, i) => (s + i, i + 1)) (0, 0))).
+  assert (pstart : P 0 (0, 0)).
+    unfold P; strong.
+  assert (pstep : forall k t, 0 <= k -> P k t ->
+    P (k + 1) ((fun '(s, j) => (s + j, j + 1)) t)).
+    intros k [s j] kge0 [kj sq].
+    split.
+      now rewrite kj.
+    strong.
+  apply Z_iter_ind; strong.
 destruct Z.iter as [v1 v2]; unfold P, fst.
-tauto.
+unfold P in main.
+strong.
 Qed.
 
 Lemma sum_n2_formula n : 0 <= n ->
   Z.iter n (fun '(s, i) => (s + i ^ 2, i + 1)) (0, 0) = 
     (n * (2 * n - 1) * (n - 1) / 6, n).
 Proof.
-intros nge0.
-set (P := fun i '(s, j) => i = j /\ s = i * (2 * i - 1) * (i - 1) / 6).
-assert (pstart : P 0 (0, 0)) by easy.
-assert(pstep :
-  forall k t, 0 <= k -> P k t -> P (k + 1) ((fun '(s, i) =>
-   (s + i ^ 2, i + 1)) t)).
-  intros k [s j] kge0 [kj sq].
-  split.
-    now rewrite kj.
-  rewrite kj.
-  lia.
-assert (Main := Z_iter_ind P _ _ pstart pstep _ nge0).
-destruct Z.iter as [v v0].
-destruct Main as [indexq sumq].
-rewrite <- indexq.
-rewrite <- sumq.
-easy.
+intros nge0; unfold sum_n', sum_n.
+set (P := fun i '(s, j) => i = j /\ s = (i * (2 * i - 1)* (i - 1) / 6)).
+assert (main : P n (Z.iter n (fun '(s, i) => (s + i ^ 2, i + 1)) (0, 0))).
+  assert (pstart : P 0 (0, 0)).
+    unfold P; strong.
+  assert (pstep : forall k t, 0 <= k -> P k t ->
+    P (k + 1) ((fun '(s, j) => (s + j ^ 2, j + 1)) t)).
+    intros k [s j] kge0 [kj sq].
+    split.
+      now rewrite kj.
+    strong.
+  apply Z_iter_ind; strong.
+destruct Z.iter as [v1 v2]; unfold P, fst.
+unfold P in main.
+destruct main as [main indexq]; rewrite <- main, indexq.
+strong.
 Qed.
 
+(* A section on the fibonacci function. *)
 Definition fib n :=
   fst (Z.iter n (fun '(fn, fnm1) => (fn + fnm1, fn)) (0, 1)).
 
@@ -107,6 +118,8 @@ Proof. easy. Qed.
 Lemma fib1 : fib 1 = 1.
 Proof. easy. Qed.
 
+(* we wish to prove that the fibonacci function satisfies the well
+  known recursive equation fib n = fib (n - 1) + fib (n - 2) *)
 Lemma fib_pair n : 1 < n ->
   Z.iter n (fun '(fn, fnm1) => (fn + fnm1, fn)) (0, 1) =
   (fst (Z.iter (n - 1) (fun '(fn, fnm1) => (fn + fnm1, fn)) (0, 1))
@@ -114,27 +127,25 @@ Lemma fib_pair n : 1 < n ->
    fst (Z.iter (n - 1) (fun '(fn, fnm1) => (fn + fnm1, fn)) (0, 1))).
 Proof.
 intros ngt1.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 1)).
-destruct (n =? 2) eqn:cmp2.
-  replace n with 2 by lia.
+induction n as [n Ih] using Z_0_le_ind.
+assert (n = 2 \/ 2 < n) as [n2 | ngt2] by strong.
+  rewrite n2.
   easy.
 pattern n at 1.
-replace n with (n - 1 + 1) by ring.
-rewrite Z_iter_succ_l;[ | lia].
-rewrite Ih;[ | lia | lia].
-replace (n - 1 - 1) with (n - 2) by ring.
-replace (n - 1 - 2) with (n - 3) by ring.
-easy.
+replace n with (n - 1 + 1) by strong.
+rewrite Z_iter_succ_l; strong.
+rewrite Ih; strong.
+replace (n - 1 - 1) with (n - 2) by strong.
+replace (n - 1 - 2) with (n - 3) by strong.
+strong.
 Qed.
 
-Lemma fib_rec n : 1 < n ->
-  fib n = fib (n - 1) + fib (n - 2).
+Lemma fib_rec n : 1 < n -> fib n = fib (n - 1) + fib (n - 2).
 Proof.
 intros ngt1.
 assert (main := fib_pair n ngt1).
 unfold fib at 1.
-rewrite main.
-easy.
+rewrite main; strong.
 Qed.
 
 Lemma every_fib :
@@ -145,59 +156,59 @@ Proof.
 intros g g0 g1 ggt1 n.
 induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
 intros nge0.
-destruct (n =? 0) eqn:cmp0.
-  replace n with 0 by lia.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0.
   apply g0.
-destruct (n =? 1) eqn:cmp1.
-  replace n with 1 by lia.
+assert (n = 1 \/ 1 < n) as [n1 | ngt1] by strong.
+  rewrite n1.
   apply g1.
-rewrite ggt1;[ | lia].
-rewrite fib_rec;[ | lia].
-rewrite Ih;[ | lia | lia].
-rewrite Ih; [ | lia | lia].
-easy.
+rewrite ggt1; strong.
+rewrite fib_rec; strong.
+rewrite Ih; strong.
+rewrite Ih; strong.
 Qed.
 
+(* A section about the binomial function. *)
 Definition bin (n : Z) :=
   Z.iter n (fun f m => f m + f (m - 1))
     (fun x => if x =? 0 then 1 else 0).
 
 Lemma bin0 m : bin 0 m = if m =? 0 then 1 else 0.
-Proof. easy. Qed.
+Proof. strong. Qed.
 
 Lemma bin_rec n m: 0 < n -> bin n m = bin (n - 1) m + bin (n - 1) (m - 1).
 Proof.
 intros ngt0.
 replace n with (n - 1 + 1) at 1 by ring.
-unfold bin; rewrite Z_iter_succ_l; lia.
+unfold bin; rewrite Z_iter_succ_l; strong.
 Qed.
 
 Lemma bin_outside n m : 0 <= n -> (m < 0 \/ n < m) ->
   bin n m = 0.
 Proof.
 revert m.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+induction n as [n Ih] using Z_0_le_ind.
 intros m nge0.
-destruct (n =? 0) eqn:cmp0.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
   intros mplaces.
-  replace n with 0 by lia.
+  rewrite n0.
   simpl.
-  replace (m =? 0) with false by lia.
-  easy.
-rewrite bin_rec;[ | lia].
+  (* A specific behavior inherited from lia that requires explanation. *)
+  replace (m =? 0) with false by strong.
+  strong.
+rewrite bin_rec; strong.
 intros mconds.
-assert (mcond1 : m < 0 \/ n - 1 < m) by lia.
-assert (mcond2 : m -1 < 0 \/ n - 1 < m - 1) by lia.
-rewrite Ih;[ | lia | lia |easy ].
-rewrite Ih;[ | lia | lia | easy].
-ring.
+rewrite Ih; strong.
+rewrite Ih; strong.
 Qed.
 
+(* Definition of the factorial function.  We are using a
+  for i in (1 .. n) pattern, as already seen in sum_n' *)
 Definition fact (n : Z) :=
   fst (Z.iter n (fun '(p, i) => (p * i, i + 1)) (1, 1)).
 
 Lemma fact0 : fact 0 = 1.
-Proof. easy. Qed.
+Proof. strong. Qed.
 
 Lemma fact_rec n :  0 < n -> fact n = fact (n - 1) * n.
 Proof.
@@ -205,115 +216,116 @@ intros ngt0.
 assert (main : forall m, 0 <= m -> 
   snd (Z.iter m (fun '(p, i) => (p * i, i + 1)) (1, 1)) = m + 1).
   set (P := fun i (t : Z * Z) => snd t = i + 1).
-  assert (pstart : P 0 (1, 1)) by easy.
+  assert (pstart : P 0 (1, 1)) by strong.
   assert (pstep : forall k t, 0 <= k -> P k t -> P (k + 1)
             ((fun '(p, i) => (p * i, i + 1)) t)).
     intros k [p i]; unfold P; simpl; intros kge0 Ih.
-    lia.
+    strong.
   apply (Z_iter_ind P (1, 1) _ pstart pstep).
-assert (ge0 : 0 <= n - 1) by lia.
-replace n with (n - 1 + 1) at 1 by ring.
-unfold fact; rewrite Z_iter_succ_l;[ | lia].
+replace n with (n - 1 + 1) at 1 by strong.
+unfold fact; rewrite Z_iter_succ_l; strong.
+(* There should be a specific induction theorem for the
+  for i in (1 .. n) pattern. *)
+assert (ge0 : 0 <= n - 1) by strong.
 assert (main':= main (n - 1) ge0).
+(* The specific need for simpl is unpleasant here. *)
 destruct (Z.iter (n - 1)) as [p i]; simpl in main' |- *.
-lia.
+strong.
 Qed.
 
 Lemma fact_gt0 : forall n, 0 <= n -> 0 < fact n.
 Proof.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+induction n as [n Ih] using Z_0_le_ind.
 intros nge0.
-destruct (n =? 0) eqn:cmp0.
-  replace n with 0 by lia.
-  rewrite fact0.
-  lia.
-rewrite fact_rec;[ | lia].
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0, fact0; strong.
+rewrite fact_rec; strong.
 assert (0 < fact (n - 1)).
-  apply Ih; lia.
-lia.
+  apply Ih; strong.
+strong.
 Qed.
 
 Lemma bin_n_n n : 0 <= n -> bin n n = 1.
 Proof.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+induction n as [n Ih] using Z_0_le_ind.
 intros nge0.
-destruct (n =? 0) eqn:cmpn0.
-  replace n with 0 by lia.
-  easy.
-rewrite bin_rec;[ | lia].
-rewrite bin_outside;[ | lia | lia].
-rewrite Ih;[ | lia | lia].
-ring.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0; strong.
+rewrite bin_rec; strong.
+rewrite bin_outside; strong.
+rewrite Ih; strong.
 Qed.
 
 Lemma bin_n_0 n : 0 <= n -> bin n 0 = 1.
 Proof.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+induction n as [n Ih] using Z_0_le_ind.
 intros nge0.
-destruct (n =? 0) eqn:cmpn0.
-  replace n with 0 by lia.
-  easy.
-rewrite bin_rec;[ | lia].
-  rewrite Ih;[ | lia | lia].
-rewrite bin_outside; lia.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0; strong.
+rewrite bin_rec; strong.
+rewrite (bin_outside _ (0 - 1)); strong.
+rewrite Ih; strong.
 Qed.
 
+(* Here is the main lemma we are interested in. *)
 Lemma bin_and_fact n m : 0 <= m <= n ->
   bin n m = fact n / (fact m * fact (n - m)).
 Proof.
-intros mnge0.
-enough (no_div : (fact m * fact (n - m) * bin n m = fact n)).
-  assert (0 < fact m) by (apply fact_gt0; lia).
-  assert (0 < fact (n - m)) by (apply fact_gt0; lia).
-  rewrite <- no_div.
-  rewrite <- (Z.mul_comm (bin n m)).
-  rewrite Z.div_mul.
-    easy.
-  lia.
-revert m mnge0.
-induction n as [n Ih] using (well_founded_ind (Z.lt_wf 0)).
+revert m.
+(* reasoning about division is especially difficult, because
+  (a + b) / c = a / c + b / c only when either a or b is divisible by c *)
+enough (main : forall m, 0 <= m <= n -> fact m * fact (n - m) * bin n m = fact n).
+  intros m mnge0.
+  rewrite <- (main m); strong.
+  Search ((_ * ?x) / ?x).
+  replace (fact m * fact (n - m) * bin n m) with (bin n m * (fact m * fact (n - m)))
+    by strong.
+  rewrite Z.div_mul; strong.
+  enough (0 < fact m /\ 0 < fact (n - m)) by strong.
+  assert (0 < fact m) by (apply fact_gt0; strong).
+  assert (0 < fact (n - m)) by (apply fact_gt0; strong).
+  strong.
+(* Reducing to a common denominator is a pain when considering integer
+  division. *)
+induction n as [n Ih] using Z_0_le_ind.
 intros m mnge0.
-destruct (n =? 0) eqn:cmp0.
-  replace n with 0 by lia.
-  replace m with 0 by lia.
-  replace (0 - 0) with 0 by ring.
+assert (n = 0 \/ 0 < n) as [n0 | ngt0] by strong.
+  rewrite n0.
+  assert (m0 : m = 0) by strong.
+  rewrite m0; strong.
+assert (m = 0 \/ 0 < m) as [m0 | mgt0] by strong.
+  rewrite m0, fact0.
+  replace (n - 0) with n by strong.
+  replace (1 * fact n) with (fact n) by strong.
+  rewrite bin_n_0; strong.
+assert (m = n \/ m < n) as [mn | mltn] by strong.
+  rewrite mn.
+  replace (n - n) with 0 by strong.
   rewrite fact0.
-  simpl; easy.
-destruct (n =? m) eqn:cmpmn.
-  replace n with m by lia.
-  rewrite bin_n_n;[ | lia].
-  replace (m - m) with 0 by ring.
-  rewrite fact0.
-  ring.
-destruct (m =? 0) eqn:cmpm0.
-  replace m with 0 by lia.
-  replace (n - 0) with n by ring.
-  rewrite bin_n_0;[ | lia].
-  rewrite fact0.
-  ring.
-rewrite bin_rec;[ | lia].
-set (facts := fact m * fact (n - m)).
-replace (facts * (bin (n - 1) m + bin (n - 1) (m - 1)))
-  with (facts * bin (n - 1) m + facts * bin (n - 1) (m - 1)) by ring.
-replace (facts * bin (n - 1) m) with
-  ((n - m) * (fact m * fact (n - 1 - m) * bin (n - 1) m)); cycle 1.
+  replace (fact n * 1) with (fact n) by strong.
+  rewrite bin_n_n; strong.
+rewrite bin_rec; strong.
+set (facts := fact m * (fact (n - m))).
+replace (facts * (bin (n - 1) m + bin (n - 1) (m - 1))) with
+  (facts * bin (n - 1) m + facts * bin (n - 1) (m - 1)) by strong.
+assert (main1 : facts * bin (n - 1) m = (n - m) * fact (n - 1)).
   unfold facts.
-  rewrite (fact_rec (n - m));[ | lia].
-  replace (n - m - 1) with (n - 1 - m) by ring.
-  ring.
-rewrite Ih;[ | lia | lia].
-replace (facts * bin (n - 1) (m - 1)) with
-  (m * (fact (m - 1) * fact (n - 1 - (m - 1)) * bin (n - 1) (m - 1)));
-  cycle 1.
+  rewrite (fact_rec (n - m)); strong.
+  replace (n - m - 1) with (n - 1 - m) by strong.
+  replace (fact m * (fact (n - 1 - m) * (n - m)) * bin (n - 1) m)
+    with ((n - m) * (fact m * fact (n - 1 - m) * bin (n - 1) m)) by strong.
+  rewrite Ih; strong.
+assert (main2 : facts * bin (n - 1) (m - 1) = m * fact (n - 1)).
   unfold facts.
-  replace (n - m) with (n - 1 - (m - 1)) by ring.
-  replace (fact m) with (m * fact (m - 1)).
-    ring.
-  rewrite (fact_rec m);[ | lia].
-  ring.
-rewrite Ih;[ | lia | lia].
-rewrite (fact_rec n);[ | lia].
-ring.
+  rewrite (fact_rec m); strong.
+  replace (fact (m - 1) * m * fact (n - m) * bin (n - 1) (m - 1))
+    with (m * (fact (m - 1) * fact (n - m) * bin (n - 1) (m - 1))) by strong.
+  replace (n - m) with (n - 1 - (m - 1)) by strong.
+  rewrite Ih; strong.
+  rewrite main1, main2.
+  replace ((n - m) * fact (n - 1) + m * (fact (n - 1))) with
+    (n * fact (n - 1)) by strong.
+  rewrite (fact_rec n); strong.
 Qed.
 
 
